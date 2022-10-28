af0922f6e77f937182bbe69bbe254faa387d5462
Jonathan Jackson <theonethatis720@gmail.com>
2022-10-28 00:59:40 -0700

    Implemented framebuffer_size_callback Implemented GLFW window resize callback to update the OpenGL viewport automatically upon resizing the window

--- projects/CopiumRenderer/Source/CopiumRenderer/Window.cpp
+++ projects/CopiumRenderer/Source/CopiumRenderer/Window.cpp
@@ -3,11 +3,18 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+// Callbacks
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 namespace Copium {
 	Window::Window(const char* title, int width, int height) : window(nullptr), title(title), width(width), height(height) {
-		glewExperimental = GL_TRUE;
 		if(!glfwInit())
 			std::cout << "GLFW initialization failed!" << std::endl;
+
+		glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+		//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // OpenGL 3.3
+		//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Not legacy OpenGL
 
 		window = glfwCreateWindow(width, height, title, NULL, NULL);
 
@@ -18,19 +25,26 @@ namespace Copium {
 
 		glfwMakeContextCurrent(window);
 
+		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+		glewExperimental = GL_TRUE;
 		if(glewInit() != GLEW_OK) {
 			std::cout << "GLEW initialization failed!" << std::endl;
 		}
+
+		glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE); // Guarantees that a keypress is registered when pressed
+
+		glViewport(0, 0, width, height); // Tell OpenGL how big our window is
 	}
 
 	void Window::Render() {
 		glClear(GL_COLOR_BUFFER_BIT);
-	
-		glBegin(GL_TRIANGLES);
-			glVertex2f(-0.5f, -0.5f);
-			glVertex2f(0.f, 0.5f);
-			glVertex2f(0.5f, -0.5f);
-		glEnd();
+		
+			glBegin(GL_TRIANGLES);
+				glVertex2f(-0.5f, -0.5f);
+				glVertex2f(0.f, 0.5f);
+				glVertex2f(0.5f, -0.5f);
+			glEnd();
 	
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -44,4 +58,9 @@ namespace Copium {
 		return glfwWindowShouldClose(window);
 	}
 
+}
+
+/* A GLFW callback function that updates the OpenGL viewport when the GLFW window is resized */
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width/2, height);
 }
